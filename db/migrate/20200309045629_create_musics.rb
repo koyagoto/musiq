ae3c299196b222509ab7b802f955ec85943f22ec
後藤晃也 <koyagoto@gotouakiranarinoMacBook-Air.local>
2020-03-10 08:53:33 +0900

    [add]カラム追加

--- db/migrate/20200309045629_create_musics.rb
+++ db/migrate/20200309045629_create_musics.rb
@@ -2,6 +2,11 @@ class CreateMusics < ActiveRecord::Migration[5.2]
   def change
     create_table :musics do |t|
 
+      t.string :singer
+      t.string :album
+      t.text :introduction
+
+
       t.timestamps
     end
   end
