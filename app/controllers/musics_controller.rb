e237dcebd00b57d6c95283b06e2bf5146c7ece34
後藤晃也 <koyagoto@gotouakiranarinoMacBook-Air.local>
2020-03-12 18:50:20 +0900

    [add]プレビュー機能

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -18,6 +18,25 @@ class MusicsController < ApplicationController
 	 end
 	end
 
+	def edit
+		@music = MUsic.find(params[:id])
+	end
+
+	def update
+        @music = Music.find(params[:id])
+     if @music.update(music_params)
+        redirect_to musics_path
+     else
+        render 'edit'
+     end
+    end
+
+    def destroy
+       music = Music.find(params[:id])
+       music.destroy
+       redirect_to musics_path(@music)
+    end
+
 	private
     def music_params
         params.require(:music).permit(:singer, :album, :album_image, :introduction, :user_id, :rate)
