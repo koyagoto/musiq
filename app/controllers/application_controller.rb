a81a63515e8f213b31b55ea2070caecc52485c9a
後藤晃也 <koyagoto@gotouakiranarinoMacBook-Air.local>
2020-03-21 10:13:32 +0900

    [fix]ユーザー新規登録内容変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:profile_image,:introduction])
   end
 end
